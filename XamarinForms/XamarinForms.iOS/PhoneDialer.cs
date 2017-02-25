b95588a117e6bbdab6ccbfbda35fbe8ac49282eb
moerphi88 <moerphi@gmx.de>
2017-02-25 19:46:52 +0100

    Xamarin.Forms Quickstart Guide durchgeführt - second commit for missed files

---
+++ XamarinForms/XamarinForms.iOS/PhoneDialer.cs
@@ -0,0 +1,17 @@
+﻿using Foundation;
+using XamarinForms.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace XamarinForms.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
