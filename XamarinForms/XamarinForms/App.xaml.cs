524594dcf9fc0c0042e9a2bf25aa4f7f13d5353c
moerphi88 <moerphi@gmx.de>
2017-02-25 22:33:12 +0100

    added a custom ImageResourceExtension to load embedded images directly from xaml

--- XamarinForms/XamarinForms/App.xaml.cs
+++ XamarinForms/XamarinForms/App.xaml.cs
@@ -13,10 +13,10 @@ namespace XamarinForms
         {
             InitializeComponent();
             Employees = new List<Employee>();
-            Employees.Add(new Employee { ImageUri = "http://ak.scr.imgfarm.com/cats/md/SuperStock_1566-0148948.jpg", DisplayName = "Katze 1", Twitter = "sjdksjd" });
+            Employees.Add(new Employee { ImageUri = "XamarinForms.kitten1.jpg", DisplayName = "Katze 1", Twitter = "sjdksjd" });
             Employees.Add(new Employee { ImageUri = "http://ak.scr.imgfarm.com/cats/md/SuperStock_1566-0148948.jpg", DisplayName = "Katze 2", Twitter = "sjdksjd" });
             Employees.Add(new Employee { ImageUri = "http://ak.scr.imgfarm.com/cats/md/SuperStock_1566-0148948.jpg", DisplayName = "Katze 3", Twitter = "sjdksjd" });
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new CallHistoryPage());
         }
 
         protected override void OnStart()
