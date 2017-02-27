ede29ccfa58607cdfbc0746c50772673e61352f5
moerphi88 <moerphi@gmx.de>
2017-02-27 22:49:07 +0100

    Added BugKillerView + ViewModel

---
+++ XamarinForms/XamarinForms/ViewModels/BugKillerViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using XamarinForms.Services;
+
+namespace XamarinForms.ViewModels
+{
+    public class BugKillerViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Kegler> _names;
+
+        public ObservableCollection<Kegler> Names
+        {
+            get
+            {
+                return _names;
+            }
+
+            set
+            {
+                _names = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BugKillerViewModel(DataService _dataService)
+        {
+            var dataService = _dataService;
+
+            Names = dataService.GetNames();
+        }
+
+ 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms/Views/BugKillerView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinForms.Services;
+using XamarinForms.ViewModels;
+
+namespace XamarinForms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BugKillerView : ContentPage
+    {
+        DataService dataService;
+
+        public BugKillerView(DataService _dataService)
+        {
+            InitializeComponent();
+            dataService = _dataService;
+            BindingContext = new BugKillerViewModel(_dataService);
+        }
+
+        //Todo: Das muss noch als Command ins ViewModel gezogen werden!
+        async void OnClickAddKegler(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddKeglerView(dataService));
+        }
+    }
+}
